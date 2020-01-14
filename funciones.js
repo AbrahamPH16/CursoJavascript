af970557987f8c60e3de919868a7aa0e00e8cbca
Anonimo <abrahamph16@gmail.com>
2020-01-14 12:39:56 -0600

    Aprendi a hacer funciones

---
+++ funciones.js
@@ -0,0 +1,12 @@
+var nombre = 'Abraham', edad = 28
+
+function imprimiredad(n,e){
+    console.log(`${n} tiene ${e} años`)
+}
+
+imprimiredad('Goku',100)
+imprimiredad('Apex',3)
+imprimiredad('Maicra',10000)
+imprimiredad('Pollo',20)
+
+
