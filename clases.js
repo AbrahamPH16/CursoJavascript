37422870ebe74cb626cf6ef8381c18600b3a806e
Abraham <abrahamph16@gmail.com>
2020-01-20 23:02:03 -0600

    Funciones como parametros

--- clases.js
+++ clases.js
@@ -12,7 +12,13 @@ class Persona {
         this.apellido = apellido
         this.alturas = altura
      }
-     saludar(){console.log(`Hola me llamo ${this.nombre} ${this.apellido}`)}
+     saludar(fn){
+          var {nombre,apellido} = this
+          console.log(`Hola me llamo ${nombre} ${apellido}`)
+          if(fn){
+               fn(nombre,apellido)
+          }
+     }
      soyAlto(){return this.altura > 1.6}
 }
 
@@ -20,8 +26,31 @@ class Desarrollador extends Persona{
      constructor (nombre, apellido,altura){
           super(nombre,apellido,altura)
      }
-     saludar (){ console.log(`Hola me llamo ${this.nombre} y soy desarrolador`)}
+     saludar (fn){ 
+          var {nombre,apellido} = this
+          console.log(`Hola me llamo ${nombre} y soy desarrolador`)
+          if(fn){
+               fn(nombre,apellido,true)
+          }
+     }
 }
+
+
+
+function responderSaludo(nombre, apellido, esDev){
+     console.log(`Buen dia ${nombre} ${apellido}`)
+     if (esDev) {
+          console.log(`ah mira, no sabia que eras desarrollador`)
+     }
+}
+
+var Krillin = new Persona('Krillin','Pelon',1.50)
+var Goku = new Desarrollador('Goku','Prota',1.70)
+var Vegeta = new Persona('Vegeta','Secundario',1.70)
+
+Krillin.saludar();
+Goku.saludar(responderSaludo);
+Vegeta.saludar(responderSaludo);
 
 
 // function Persona(nombre, apellido,altura){
@@ -41,9 +70,7 @@ class Desarrollador extends Persona{
 
 
 
-//var Krillin = new Persona('Krillin','Pelon',1.50)
-//var Goku = new Persona('Goku','Prota',1.70)
-//var Vegeta = new Persona('Vegeta','Secundario',1.70)
+
 
 //Krillin.saludar()
 
