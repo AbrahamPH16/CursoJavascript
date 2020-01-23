f3dfdbabb76c2918722b169f82aaf8de5a88e3b0
Abraham <abrahamph16@gmail.com>
2020-01-22 22:51:32 -0600

    Infierno de CallBack

--- Request.js
+++ Request.js
@@ -4,16 +4,31 @@ const PEOPLE_URL = 'people/:id'
 
 const opts = {crossDomain: true}
 
-const onResponse = function(persona){
-    console.log(`Hola, mi nombre es ${persona.name}`)
-}
 
-function obtenerPersonaje(id){
+
+function obtenerPersonaje(id, callback){
     const url = `${API_URL}${PEOPLE_URL.replace(':id',id)}`
-    $.get(url,opts,onResponse)
+
+    $.get(url, function(persona){
+    console.log(`Hola, mi nombre es ${persona.name}`)
+
+    if(callback){
+        callback()
+    }
+})
 }
 
 //Asincronismo locoooooooooooooo!!!!!!
-obtenerPersonaje(1)
-obtenerPersonaje(2)
-obtenerPersonaje(3)
+//Con callback perdemos el paralelismo del request
+//obtenerPersonaje(1)
+//obtenerPersonaje(2)
+//obtenerPersonaje(3)
+
+//El infierno de los callback Muajajaja
+obtenerPersonaje(1, function() {
+    obtenerPersonaje(2, function () {
+        obtenerPersonaje(3, function () {
+            obtenerPersonaje(4)
+        })
+    })
+})
