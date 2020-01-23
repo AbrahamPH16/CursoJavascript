67e46ce88234a02158f24dd742a0c082c3b51552
Abraham <abrahamph16@gmail.com>
2020-01-22 22:38:36 -0600

    Asincronismo en su maximo explendor

--- Request.js
+++ Request.js
@@ -2,9 +2,18 @@ const API_URL = 'https://swapi.co/api/'
 
 const PEOPLE_URL = 'people/:id'
 
-const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id',1)}`
 const opts = {crossDomain: true}
 
-$.get(lukeUrl,opts, function(){
-    console.log(arguments)
-})
+const onResponse = function(persona){
+    console.log(`Hola, mi nombre es ${persona.name}`)
+}
+
+function obtenerPersonaje(id){
+    const url = `${API_URL}${PEOPLE_URL.replace(':id',id)}`
+    $.get(url,opts,onResponse)
+}
+
+//Asincronismo locoooooooooooooo!!!!!!
+obtenerPersonaje(1)
+obtenerPersonaje(2)
+obtenerPersonaje(3)
